9e66224db133bfad0957640a91d19162830079b5
tatianazapata88 <taty0611z@gmail.com>
2021-08-19 14:55:34 -0500

    ajuste codesmells

--- microservicio/infraestructura/src/main/java/com/ceiba/compra/adaptador/repositorio/RepositorioCompraMysql.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/compra/adaptador/repositorio/RepositorioCompraMysql.java
@@ -13,7 +13,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-import java.lang.NullPointerException;
 import java.util.Objects;
 
 @Repository
