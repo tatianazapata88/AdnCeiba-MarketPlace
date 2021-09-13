2bc08363d39df45502f0089f53dfc17f29756725
tatianazapata88 <taty0611z@gmail.com>
2021-09-12 20:53:48 -0500

    ajuste back

--- microservicio/infraestructura/src/test/java/com/ceiba/scotter/controlador/ConsultaControladorScotterTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/scotter/controlador/ConsultaControladorScotterTest.java
@@ -43,7 +43,7 @@ public class ConsultaControladorScotterTest {
         mocMvc.perform(get("/scotters/id/{id}", id)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("marca", is("toyota")));
+                .andExpect(jsonPath("marca", is("enduro")));
     }
 
     @Test
@@ -76,6 +76,6 @@ public class ConsultaControladorScotterTest {
         mocMvc.perform(get("/scotters/vendedor/{vendedor}", vendedor)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("marca", is("toyota")));
+                .andExpect(jsonPath("marca", is("enduro")));
     }
 }
